7b2f8e0378468e3ea1c9eb51c92a6aad1713b9a6
thelakeviewaframe <jkrealestate221@gmail.com>
2026-09-16 10:50:39 -0600

    Add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,26 @@
+const SITE_URL = 'https://www.thelakeviewaframe.com';
+
+export default function sitemap() {
+  const lastModified = new Date();
+
+  return [
+    {
+      url: `${SITE_URL}/`,
+      lastModified,
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: `${SITE_URL}/things-to-do`,
+      lastModified,
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${SITE_URL}/about`,
+      lastModified,
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+  ];
+}
